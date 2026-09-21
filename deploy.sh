88896d777a9ddf6ddf68a3c4f512bf236da52b47
Swaraj <swarajchouriwar@gmail.com>
2026-09-21 11:45:59 +0530

    ECR Updated

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 set -e
 
 REPO_DIR="/usr/bin/zeyro/astra-backend"
-# Use the Docker Hub username provided by the user in the k8s manifests
-IMAGE="fieryice24/astra-backend-api"
+# Use the ECR URL provided by the user
+IMAGE="070443470895.dkr.ecr.ap-south-1.amazonaws.com/abhimanyu-gupta-ecr"
 
 # We check if the script is running in the correct directory, if not, we try to CD into it
 if [ ! -d "$REPO_DIR" ]; then
@@ -20,13 +20,22 @@ git pull origin main
 # Get the short git commit hash for tagging
 TAG=$(git rev-parse --short HEAD)
 
+echo "==> Authenticating with AWS ECR"
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 070443470895.dkr.ecr.ap-south-1.amazonaws.com
+
+echo "==> Downloading AWS RDS SSL Certificate"
+curl -sS -o global-bundle.pem https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+
 echo "==> Building image: $IMAGE:$TAG"
 docker build -t "$IMAGE:$TAG" .
 
 echo "==> Pushing image"
 docker push "$IMAGE:$TAG"
 
-echo "==> Updating Kubernetes deployment"
+echo "==> Applying Kubernetes configurations (ConfigMaps, Secrets, Services, Deployment)"
+kubectl apply -f k8s/
+
+echo "==> Updating Kubernetes deployment with new image tag"
 kubectl set image deployment/astra-backend \
   astra-backend="$IMAGE:$TAG"
 
